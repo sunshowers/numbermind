9d326c9840c3f93560a914ece1f68e40b903ac09
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-05-14 01:29:01 +0530

    Hook everything up. Works now.

--- server.rkt
+++ server.rkt
@@ -1,23 +1,32 @@
 #lang racket
 
 (require (planet dherman/json:4:0))
-(require "utils.rkt")
-(require web-server/servlet
+(require "numbermind.rkt"
+         "utils.rkt")
+(require srfi/13
+         web-server/servlet
          web-server/servlet-env)
 
 (define num-digits (make-parameter 0))
 
 (define (send-next-guess request guess)
-  (define handler (λ (req) (handle-guess-response req)))
+  (define handler (λ (req) (handle-guess-response guess req)))
   (define (response-generator embed/url)
     (response/json
      `#hash((nextGuess . ,guess)
             (addGuessURL . ,(embed/url handler)))))
   (send/suspend/dispatch response-generator))
 
-(define (handle-guess-response request)
+(define (handle-guess-response guess request)
   (define data (json-request->jsexpr request))
-  (send-next-guess request (random (expt 10 (num-digits)))))
+  (define correct-digits (hash-ref data 'currentCorrectDigits))
+  (unless (and
+           (exact-nonnegative-integer? correct-digits)
+           (>= correct-digits 0)
+           (<= correct-digits (num-digits)))
+    (raise (exn:fail:contract "Input not in the correct range" (current-continuation-marks))))
+  (add-guess guess correct-digits)
+  (send-next-guess request (get-new-guess)))
 
 (define (start request)
   (define data (json-request->jsexpr request))
@@ -28,7 +37,10 @@
            (<= input-num-digits 10))
     (raise (exn:fail:contract "Input should be a number" (current-continuation-marks))))
   (parameterize ([num-digits input-num-digits])
-    (send-next-guess request (random (expt 10 input-num-digits)))))
+    (with-new-numbermind
+     input-num-digits
+     (send-next-guess request
+                      (string-pad (number->string (random (expt 10 input-num-digits))) input-num-digits #\0)))))
 
 (serve/servlet start
                #:launch-browser? #f
