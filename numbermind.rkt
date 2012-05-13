d6727339217914dd801d260538dce9c2ceba17c2
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-05-13 22:06:04 +0530

    Start supporting the AJAX we want to support

--- numbermind.rkt
+++ numbermind.rkt
@@ -1,8 +1,30 @@
-#lang web-server/insta
+#lang racket
 
+(require (planet dherman/json:4:0))
 (require "utils.rkt")
-(static-files-path "html")
+(require web-server/servlet
+         web-server/servlet-env)
 
-(define (start req)
-  (response/json
-   '(1 2 3)))
+(define (handle-guess num-digits guesses request)
+  (local [(define (response-generator embed/url)
+            (response/json
+             `#hash((nextGuess . ,(random (expt 10 num-digits)))
+                    (addGuessURL . ,(embed/url (λ (req) (handle-guess num-digits guesses req)))))))]
+    (send/suspend/dispatch response-generator)))
+
+(define (start request)
+  (define json-data (bytes->string/utf-8 (request-post-data/raw request)))
+  (define data (read-json (open-input-string json-data)))
+  (define num-digits (hash-ref data 'numDigits))
+  (unless (and
+           (exact-nonnegative-integer? num-digits)
+           (>= num-digits 1)
+           (<= num-digits 10))
+    (raise (exn:fail:contract "Input should be a number" (current-continuation-marks))))
+  (handle-guess num-digits '() request))
+
+(serve/servlet start ; answers requests
+               #:launch-browser? #f
+               #:servlet-path "/numbermind" ; is the default URL
+               #:extra-files-paths (list "html")
+               #:port 8080) ; is the port
